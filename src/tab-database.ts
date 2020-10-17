577c0531448576921da7340c7b2d6323d9e917dc
Otto <61689180+prOttonicFusion@users.noreply.github.com>
2020-10-18 00:11:28 +0300

    add tab DB service

---
+++ src/tab-database.ts
@@ -0,0 +1,62 @@
+import DataBaseAccess from "./data-access";
+
+class TabDB {
+  dataAccess: DataBaseAccess;
+
+  constructor(dataAccess: DataBaseAccess) {
+    this.dataAccess = dataAccess;
+  }
+
+  init(): Promise<boolean> {
+    const sqlQuery1 = `
+      CREATE TABLE IF NOT EXISTS tab (name TEXT, balance REAL)`;
+    const sqlQuery2 = `
+      CREATE TABLE IF NOT EXISTS history (name TEXT, timestamp TEXT, transact REAL)`;
+
+    return this.dataAccess.run(sqlQuery1) && this.dataAccess.run(sqlQuery2);
+  }
+
+  addUser(name: string, initialBalance: number): Promise<boolean> {
+    return (
+      this.dataAccess.run("INSERT INTO tab (name, balance) VALUES (?, ?)", [
+        name,
+        initialBalance,
+      ]) && this.dataAccess.run("INSERT INTO history (name) VALUES (?)", [name])
+    );
+  }
+
+  deleteUser(name: string): Promise<boolean> {
+    return (
+      this.dataAccess.run(`DELETE FROM tab WHERE name=?`, [name]) &&
+      this.dataAccess.run(`DELETE FROM history WHERE name=?`, [name])
+    );
+  }
+
+  updateUser(name: string, newBalance: number): Promise<boolean> {
+    return this.dataAccess.run(`UPDATE tab SET balance=? WHERE name=?`, [
+      newBalance,
+      name,
+    ]);
+  }
+
+  updateHistory(
+    name: string,
+    timeStamp: string,
+    transaction: number
+  ): Promise<boolean> {
+    return this.dataAccess.run(
+      `INSERT INTO history (name, timestamp, transact) VALUES (?, ?, ?)`,
+      [name, timeStamp, transaction]
+    );
+  }
+
+  getUserNames(): Promise<any> {
+    return this.dataAccess.getAll(`SELECT name FROM tab`);
+  }
+
+  getBalanceOfUser(user: string): Promise<number> {
+    return this.dataAccess.get(`SELECT balance FROM tab WHERE name=?`, [user]);
+  }
+}
+
+export default TabDB;
