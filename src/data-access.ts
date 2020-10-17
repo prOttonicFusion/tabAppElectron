28c1479f6ba86d58bb8455df7a6cc91867318bf6
Otto <61689180+prOttonicFusion@users.noreply.github.com>
2020-10-17 23:12:55 +0300

    add data getters

--- src/data-access.ts
+++ src/data-access.ts
@@ -27,6 +27,34 @@ class DataBaseAccess {
       });
     });
   }
+
+  get(sqlQuery: string, params: any[] = []): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.db.get(sqlQuery, params, (err, result) => {
+        if (err) {
+          console.log("Error running SQL query: " + sqlQuery);
+          console.log(err);
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  }
+
+  getAll(sqlQuery: string, params: any[] = []): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.db.all(sqlQuery, params, (err, rows) => {
+        if (err) {
+          console.log("Error running SQL query: " + sqlQuery);
+          console.log(err);
+          reject(err);
+        } else {
+          resolve(rows);
+        }
+      });
+    });
+  }
 }
 
 export default DataBaseAccess;
