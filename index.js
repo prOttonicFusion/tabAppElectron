575f0afe64a467f350978342edd609c88cdeaea7
Otto <61689180+prOttonicFusion@users.noreply.github.com>
2020-10-18 01:39:17 +0300

    get form input

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-// import { ipcRenderer } from "electron";
-const { ipcRenderer } = require('electron');
+const { ipcRenderer } = require("electron");
 
 document.getElementById("add-user-button").addEventListener("click", () => {
   ipcRenderer.send("add-user");
 });
 
 document.getElementById("accept-button").addEventListener("click", () => {
-    ipcRenderer.send("accept-transaction");
-  });
+  let transaction = getTransactionValue();
+  if (transaction) {
+    ipcRenderer.send("accept-transaction", [transaction]);
+  }
+  resetInputFields();
+});
+
+function getTransactionValue() {
+  let price = Number(document.getElementById("price-input").value);
+  let deposit = Number(document.getElementById("recharge-input").value);
+  var value = 0.0;
+  if (price) {
+    value -= price;
+  }
+  if (deposit) {
+    value += deposit;
+  }
+  return value;
+}
+
+function resetInputFields() {
+  document.getElementById("recharge-input").value = "";
+  document.getElementById("price-input").value = "";
+}
