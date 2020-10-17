c4eff7edd4dcfffb7ba1c17ccfc7695574f508f8
Otto <61689180+prOttonicFusion@users.noreply.github.com>
2020-10-18 00:18:27 +0300

    solve db connection issue

--- src/data-access.ts
+++ src/data-access.ts
@@ -5,7 +5,7 @@ class DataBaseAccess {
   db: Database;
 
   constructor(dbFilePath: string) {
-    this.db = new Database(dbFilePath, OPEN_CREATE, (err) => {
+    this.db = new Database(dbFilePath, (err) => {
       if (err) {
         console.log("Could not connect to database", err);
       } else {
@@ -14,7 +14,7 @@ class DataBaseAccess {
     });
   }
 
-  run(sqlQuery: string, params = []): Promise<boolean> {
+  run(sqlQuery: string, params: any[] = []): Promise<boolean> {
     return new Promise((resolve, reject) => {
       this.db.run(sqlQuery, params, function (err) {
         if (err) {
