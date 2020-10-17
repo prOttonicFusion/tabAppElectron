1830d862ee06f84423eee16457211682675a7b84
Otto <61689180+prOttonicFusion@users.noreply.github.com>
2020-10-18 00:12:18 +0300

    set up db connection

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { app, BrowserWindow } from "electron";
 import * as path from "path";
+import DataBaseAccess from "./data-access";
+import TabDB from "./tab-database";
 
 function createWindow() {
   // Create the browser window.
@@ -42,3 +44,5 @@ app.on("window-all-closed", () => {
 
 // In this file you can include the rest of your app"s specific main process
 // code. You can also put them in separate files and require them here.
+const dataAccess = new DataBaseAccess("../database.sqlite3");
+const tabDB = new TabDB(dataAccess);
