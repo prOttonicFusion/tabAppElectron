42ec2e743e78b82be24a9b436ef0076d77e61e56
Otto <61689180+prOttonicFusion@users.noreply.github.com>
2020-10-20 00:59:09 +0300

    refactor userList populating (fixes #4)

--- index.js
+++ index.js
@@ -38,9 +38,11 @@ document.getElementById("accept-button").addEventListener("click", () => {
 
 function populateUserDropdown(userList) {
   const dropdown = document.getElementById("user-selector");
-
-  for (var i = 0; i < userList.length; i++) {
-    dropdown.options.add(new Option(userList[i]));
+  for (var i = dropdown.options.length - 1; i >= 0; i--) {
+    dropdown.remove(i);
+  }
+  for (var j = 0; j < userList.length; j++) {
+    dropdown.options.add(new Option(userList[j]));
   }
 }
 
