a128bbadf87339180adaab674feee149ca7e2c12
Otto <61689180+prOttonicFusion@users.noreply.github.com>
2021-09-18 23:27:23 +0300

    lint .js files

--- src/localization/translation-selector.js
+++ src/localization/translation-selector.js
@@ -1,13 +1,15 @@
-const settings = require("../../settings.json");
+/* eslint-disable @typescript-eslint/no-var-requires */
 
-const languageFilePath = "./" + settings.language + ".json";
-var translation;
+const settings = require('../../settings.json')
+
+const languageFilePath = `./${  settings.language  }.json`
+var translation
 
 try {
-  translation = require(languageFilePath);
+    translation = require(languageFilePath)
 } catch (ex) {
-  console.log("Unable to load selected language. Falling back to default");
-  translation = require("./en.json");
+    console.log('Unable to load selected language. Falling back to default')
+    translation = require('./en.json')
 }
 
-exports.translation = translation;
+exports.translation = translation
