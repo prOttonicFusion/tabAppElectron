304d598c8996155910a7527fb35ecd2794d51915
Otto <61689180+prOttonicFusion@users.noreply.github.com>
2021-09-21 00:17:26 +0300

    fix/localization-file-reading

--- src/localization/translate-main.js
+++ src/localization/translate-main.js
@@ -1,19 +1,13 @@
-const { translation }  = require('./translation-selector')
+const translation = require('./localization/translation-selector')
 
-document.getElementById('user-selector-label').innerHTML =
-  translation['user-selector-label']
-document.getElementById('disabled-list-option').innerHTML =
-  translation['disabled-list-option']
-document.getElementById('balance-display-label').innerHTML =
-  translation['balance-display-label']
-document.getElementById('add-user-button').innerHTML =
-  translation['add-user-button']
-document.getElementById('price-input-label').innerHTML =
-  translation['price-input-label']
-document.getElementById('recharge-input-label').innerHTML =
-  translation['recharge-input-label']
-document.getElementById('accept-button').innerHTML =
-  translation['accept-button']
+document.getElementById('user-selector-label').innerHTML = translation['user-selector-label']
+document.getElementById('disabled-list-option').innerHTML = translation['disabled-list-option']
+document.getElementById('balance-display-label').innerHTML = translation['balance-display-label']
+document.getElementById('add-user-button').innerHTML = translation['add-user-button']
+document.getElementById('price-input-label').innerHTML = translation['price-input-label']
+document.getElementById('recharge-input-label').innerHTML = translation['recharge-input-label']
+document.getElementById('accept-button').innerHTML = translation['accept-button']
+
 Array.from(document.getElementsByClassName('currency-symbol')).forEach(
     element => {
         element.innerHTML = translation['currency-symbol']
--- src/localization/translation-selector.js
+++ src/localization/translation-selector.js
@@ -1,12 +1,13 @@
 const settings = require('../../settings.json')
 
 const languageFilePath = `./${  settings.language  }.json`
+
 let translation
 
 try {
     translation = require(languageFilePath)
-} catch (ex) {
-    console.log('Unable to load selected language. Falling back to default')
+} catch (err) {
+    console.warn('Unable to load selected language. Falling back to default')
     translation = require('./en.json')
 }
 
